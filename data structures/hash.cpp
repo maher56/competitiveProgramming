9b14a4a6e1e8875c095ac8ae5680ef763cd01d7e
maher56 <78613558+maher56@users.noreply.github.com>
2022-11-12 13:21:01 +0200

    Update hash.cpp

--- data structures/hash.cpp
+++ data structures/hash.cpp
@@ -1,5 +1,5 @@
 struct Hash {
-    int n,p1,p2,m1,m2,dif;
+    int n,p1,p2,m1,m2;
     pair<int,int> hash;
     string s;
     vector<int> ha1,ha2,pw1,pw2;
@@ -8,7 +8,9 @@ struct Hash {
         n = str.size();
         p1=29;p2=31;
         m1=1e9+7;m2=1e9+11;
-        dif='a'+1;
+    }
+    int dif(char x) {
+        return int(x-'a'+1);
     }
     ll Mul(ll x,ll y,ll mod=mod){R((x%mod)*(y%mod))%mod;}
     ll Add(ll x,ll y,ll mod=mod){R((x%mod)+(y%mod)+2ll*mod)%mod;}
@@ -18,12 +20,12 @@ struct Hash {
         ha1.resize(n);
         ha2.resize(n);
         pw1[0]=pw2[0]=1;
-        ha1[0]=ha2[0]=s[0]-dif;
+        ha1[0]=ha2[0]=dif(s[0]);
         for(int i = 1 ; i < n ; i++) {
             pw1[i] = Mul(pw1[i-1],p1,m1);
             pw2[i] = Mul(pw2[i-1],p2,m2);
-            ha1[i] = Add(Mul(ha1[i-1],p1,m1),(s[i]-dif),m1);
-            ha2[i] = Add(Mul(ha2[i-1],p2,m2),(s[i]-dif),m2);
+            ha1[i] = Add(Mul(ha1[i-1],p1,m1),dif(s[i]),m1);
+            ha2[i] = Add(Mul(ha2[i-1],p2,m2),dif(s[i]),m2);
         }
     }
     void build_hash() {
@@ -32,10 +34,10 @@ struct Hash {
             hash.second =ha2[n-1];
             return;
         }
-        hash.first = hash.second = s[0]-dif;
+        hash.first = hash.second = dif(s[0]);
         for(int i = 1 ; i < n ; i++) {
-            hash.first = Add(Mul(hash.first,p1,m1),(s[i]-dif),m1);
-            hash.second = Add(Mul(hash.second,p2,m2),(s[i]-dif),m2);
+            hash.first = Add(Mul(hash.first,p1,m1),dif(s[i]),m1);
+            hash.second = Add(Mul(hash.second,p2,m2),dif(s[i]),m2);
         }
     }
     pair<int,int> subHash(int l,int r) {
