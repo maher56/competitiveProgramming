b157886baf2dda0cf075e9b79ddfc71c9c85af00
maher56 <78613558+maher56@users.noreply.github.com>
2023-02-17 17:26:55 +0200

    Update sparseTable.cpp

--- data structures/sparseTable.cpp
+++ data structures/sparseTable.cpp
@@ -1,30 +1,24 @@
-struct sparseTable {
-    int mat[N][int(log2(N))+1],p2[N],m;
-    void init(int n) {
-        m = int(log2(n))+1;
-        p2[0] = p2[1] = 0;
-        for(int i = 2 ; i <= n ; i++) {
-            p2[i] = p2[i/2]+1;
+struct ST {
+    int mat[20][N],lg[N];
+    void init() {
+        lg[0]=-1;
+        for(int i = 1 ; i <= n ;i++) {
+            lg[i] = lg[i-1] + !(i&(i-1));
         }
-        for(int i = 0 ; i < n ; i++) {
-            mat[i][0] = a[i];
-        }
-        for(int j = 1 ; j < m ; j++) {
-            for(int i = 0 ; i < n ; i++) {
-                mat[i][j] = min(mat[i][j-1],mat[i+(1<<(j-1))][j-1]);
-//                mat[i][j] = mat[i][j-1] + mat[i+(1<<(j-1))][j-1]; // for sum query
+        for(int i = 0 ; i < n ; i++) mat[0][i] = i;
+        for(int i = 1 ; (1<<i) <= n ; i++) {
+            for(int j = 0 ; j + (1<<i) <= n ; j++) {
+                int fi = mat[i-1][j];
+                int se = mat[i-1][j+(1<<(i-1))];
+                mat[i][j] = a[fi]<a[se]?fi:se;
             }
         }
     }
     int query(int l,int r) {
-        int pw = p2[r-l];
-        return  min(mat[l][pw],mat[r-(1<<pw)+1][pw]);
-//        int sum = 0; // for sum query
-//        while(l<=r) {
-//            sum += mat[l][pw];
-//            l += (1<<pw);
-//            pw = p2[r-l];
-//        }
-//        return sum;
+        if(l > r) R INF;
+        int sz = lg[r-l+1];
+        int fi = mat[sz][l];
+        int se = mat[sz][r-(1<<sz)+1];
+        R min(a[fi],a[se]);
     }
 } st;
