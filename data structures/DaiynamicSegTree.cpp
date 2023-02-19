ab001a8140de1ab9a0d7d45cffe348c33e920fc8
maher56 <78613558+maher56@users.noreply.github.com>
2023-02-19 14:33:18 +0200

    Update DaiynamicSegTree.cpp

--- data structures/DaiynamicSegTree.cpp
+++ data structures/DaiynamicSegTree.cpp
@@ -1,5 +1,5 @@
 struct dai_sg {
-    ll val[30*N]lazy[30*N];
+    ll val[30*N],lazy[30*N];
     int lf[30*N],rt[30*N],node_num;
     void init() {
         node_num = 1;
@@ -62,55 +62,5 @@ struct dai_sg {
         }
         int mid=st+(en-st)/2;
         return query(qs,qe,lf[node],st,mid)+query(qs,qe,rt[node],mid+1,en);
-    }
-    // first number greater than zero in range qs qe
-    int get_l(int qs,int qe,int node=1,int st=1,int en=1e9) {
-        if(!node) R -1;
-        laz(st,en,node);
-        if(!val[node]) R -1;
-        if(st > qe || en < qs) R -1;
-        if(st == en) R val[node]?st:-1;
-        if(st >= qs && en <= qe) {
-            int mid = st+(en-st)/2;
-            int z = get_l(qs,qe,lf[node],st,mid);
-            if(z == -1) z = get_l(qs,qe,rt[node],mid+1,en);
-            R z;
-        }
-        int mid = st+(en-st)/2;
-        int lft,rit;
-        if(st > qe || mid < qs) lft=-1;
-        else {
-            lft = get_l(qs,qe,lf[node],st,mid);
-        }
-        if(mid+1 > qe || en < qs) rit = -1;
-        else {
-            rit = get_l(qs,qe,rt[node],mid+1,en);
-        }
-        R lft==-1?rit:lft;
-    }
-     // last number greater than zero in range qs qe
-    int get_r(int qs,int qe,int node=1,int st=1,int en=1e9) {
-        if(!node) R -1;
-        laz(st,en,node);
-        if(!val[node]) R -1;
-        if(st > qe || en < qs) R -1;
-        if(st == en) R val[node]?st:-1;
-        if(st >= qs && en <= qe) {
-            int mid = st+(en-st)/2;
-            int z = get_r(qs,qe,rt[node],mid+1,en);
-            if(z == -1) z = get_r(qs,qe,lf[node],st,mid);
-            R z;
-        }
-        int mid = st+(en-st)/2;
-        int lft,rit;
-        if(st > qe || mid < qs) lft=-1;
-        else {
-            lft = get_r(qs,qe,lf[node],st,mid);
-        }
-        if(mid+1 > qe || en < qs) rit = -1;
-        else {
-            rit = get_r(qs,qe,rt[node],mid+1,en);
-        }
-        R rit==-1?lft:rit;
     }
 }sg;
