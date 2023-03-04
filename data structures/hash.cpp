3a0f01ff340cefc5770f1958553c9875501d42dd
maher56 <78613558+maher56@users.noreply.github.com>
2023-03-04 02:02:49 +0200

    Update hash.cpp

--- data structures/hash.cpp
+++ data structures/hash.cpp
@@ -1,51 +1,39 @@
 struct Hash {
-    int n,p1,p2,m1,m2;
-    pair<int,int> hash;
+    int n,p1,p2,m1,m2,h1[N],h2[N],pw1[N],pw2[N];
     string s;
-    vector<int> ha1,ha2,pw1,pw2;
-    void init(string &str) {
+    void init(string &str,int _p1,int _p2,int _m1,int _m2) {
+        p1 = _p1;
+        p2 = _p2;
+        m1 = _m1;
+        m2 = _m2;
         s = str;
-        n = str.size();
-        p1=29;p2=31;
-        m1=1e9+7;m2=1e9+11;
+        n = s.size();
+        pw1[0] = pw2[0] = 1;
+        h1[0] = h2[0] = dif(s[0]);
+        for(int i = 1 ; i < n ; i++) {
+            h1[i] = (1ll * h1[i-1] * p1 + dif(s[i]))%m1;
+            h2[i] = (1ll * h2[i-1] * p2 + dif(s[i]))%m2;
+            pw1[i] = (1ll * pw1[i-1] * p1)%m1;
+            pw2[i] = (1ll * pw2[i-1] * p2)%m2;
+        }
     }
     int dif(char x) {
-        return int(x-'a'+1);
+        return x-'a'+1;
     }
-    ll Mul(ll x,ll y,ll mod=mod){R((x%mod)*(y%mod))%mod;}
-    ll Add(ll x,ll y,ll mod=mod){R((x%mod)+(y%mod)+2ll*mod)%mod;}
-    void preHash() {
-        pw1.resize(n);
-        pw2.resize(n);
-        ha1.resize(n);
-        ha2.resize(n);
-        pw1[0]=pw2[0]=1;
-        ha1[0]=ha2[0]=dif(s[0]);
-        for(int i = 1 ; i < n ; i++) {
-            pw1[i] = Mul(pw1[i-1],p1,m1);
-            pw2[i] = Mul(pw2[i-1],p2,m2);
-            ha1[i] = Add(Mul(ha1[i-1],p1,m1),dif(s[i]),m1);
-            ha2[i] = Add(Mul(ha2[i-1],p2,m2),dif(s[i]),m2);
-        }
-    }
-    void build_hash() {
-        if(!ha1.empty()) {
-            hash.first = ha1[n-1];
-            hash.second =ha2[n-1];
-            return;
-        }
-        hash.first = hash.second = dif(s[0]);
-        for(int i = 1 ; i < n ; i++) {
-            hash.first = Add(Mul(hash.first,p1,m1),dif(s[i]),m1);
-            hash.second = Add(Mul(hash.second,p2,m2),dif(s[i]),m2);
-        }
-    }
-    pair<int,int> subHash(int l,int r) {
-        int h1=ha1[r],h2=ha2[r];
+    pair<int,int> sub(int l,int r) {
+        int t1 = h1[r] , t2 = h2[r];
         if(l) {
-            h1 = Add(h1 , -1ll * Mul(ha1[l-1],pw1[r-l+1],m1),m1);
-            h2 = Add(h2 , -1ll * Mul(ha2[l-1],pw2[r-l+1],m2),m2);
+            t1 = (t1 - (1ll * h1[l-1] * pw1[r-l+1])%m1+m1)%m1;
+            t2 = (t2 - (1ll * h2[l-1] * pw2[r-l+1])%m2+m2)%m2;
         }
-        R {h1,h2};
+        return {t1,t2};
     }
-}h1,h2;
+    pair<int,int> merge(int l1,int r1,int l2,int r2) {
+        pair<int,int> t1,t2;
+        t1 = sub(l1,r1);
+        t2 = sub(l2,r2);
+        t1.first = (1ll * t1.first * pw1[r2-l2+1] + t2.first)%m1;
+        t1.second = (1ll * t1.second * pw2[r2-l2+1] + t2.second)%m2;
+        return t1;
+    }
+} h1;
